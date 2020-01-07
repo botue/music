2127b59c3af779697201b54dd90bfac7f892e05d
botue <botue@sina.com>
2020-01-07 13:22:47 +0800

    优化了样式

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,28 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var reload = browserSync.reload;
-
-// 静态服务器 + 监听 scss/html 文件
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch("./scss/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', reload);
-});
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const reload = browserSync.reload;
 
 // scss编译后的css将注入到浏览器里实现更新
 gulp.task('sass', function() {
-    return gulp.src("./scss/*.scss")
-        .pipe(sass({
-            outputStyle: 'expanded'
-        }))
-        .pipe(gulp.dest("./css"))
-        .pipe(reload({stream: true}));
+  return gulp.src("./scss/*.scss")
+      .pipe(sass({
+          outputStyle: 'expanded'
+      }))
+      .pipe(gulp.dest("./css"))
+      .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['serve']);
+function serve() {
+  browserSync.init({
+    server: './'
+  });
+
+  gulp.watch('./scss/*.scss', gulp.series('sass'));
+  gulp.watch('./*.html').on('change', reload);
+}
+
+exports.default = serve;
+
+
+
